0125e3f81be4b949908ee9a79e262b7f819a25d0
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2024-07-17 18:18:08 +0530

    installed daily.co for video calls

--- frontend/src/pages/InterviewInterface/InterviewInterface.jsx
+++ frontend/src/pages/InterviewInterface/InterviewInterface.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import VideoCall from '../../components/common/VideoCall/VideoCall';
 import Whiteboard from '../../components/common/Whiteboard/WhiteboardContainer';
 import CodeEditor from '../../components/CodeEditor/CodeEditor';
-import CodeApp from '../../components/CodeEditor/CodeApp';
+import VideoCall from '../../components/common/VideoCall/VideoCall';
+
+
 
 const InterviewInterface = () => {
   return (
-    <div className="grid grid-cols-[2.7fr_1.3fr] h-screen">
-      <div className="bg-gray-700 flex items-center justify-center p-8">
-        <div className="h-[500px]  w-full">
-            {/* <CodeApp/> */}
-            {/* <Whiteboard /> */}
+    <div className="grid pt-16 grid-cols-[2.7fr_1.3fr] h-screen w-full">
+      <div className="bg-gray-700 flex justify-center items-center p-8">
+        <div className="h-full w-full bg-gray-600">
+          {/* <Whiteboard /> */}
+          <CodeEditor/>
         </div>
       </div>
-      <div className="bg-gray-200 flex items-center justify-center p-8">
-            {/* <VideoCall /> */}
+      <div className="bg-gray-200 flex justify-center items-center p-8">
+        <VideoCall/>
       </div>
     </div>
   );
